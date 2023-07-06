94d5dd2498d3782ef9526e5330614c6e56887d87
MrXiaoM <coolxiaom95@gmail.com>
2023-07-06 18:13:00 +0800

    fix wrong powershell script

--- setup.ps1
+++ setup.ps1
@@ -5,10 +5,11 @@ Write-Output "  开发版本的版本号可以在以下链接中获取:"
 Write-Output "  https://repo.mirai.mamoe.net/snapshots/net/mamoe/mirai-core"
 Write-Output ""
 $version = Read-Host "输入版本号"
-Write-Output "注: 其中一个库下载耗时较长，请耐心等待"
-Write-Output
 
-./gradlew clearLibs pack -Dlapis.version=$version
+Write-Output "注: 其中一个库下载耗时较长，请耐心等待"
+Write-Output ""
+
+./gradlew.bat clearLibs pack -D"lapis.version=$version"
 
 Write-Output ""
 Write-Output "  若提示 BUILD SUCCESSFUL, 一键包已成功打包到 ./build/lapis 文件夹中."
