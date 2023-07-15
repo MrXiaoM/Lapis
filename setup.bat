0de1eae46d7cae8e536606e36eedeee9b9caa32d
MrXiaoM <coolxiaom95@gmail.com>
2023-07-15 12:59:57 +0800

    bat 改用英文，避免乱码

--- setup.bat
+++ setup.bat
@@ -1,23 +1,20 @@
-chcp 65001
 @echo off
-cls
-title Mirai一键包打包器 - Lapis
+title Mirai OneKey Installer - Lapis
 echo.
-echo.  在运行本程序之前, 请确保你已经安装 java.
-echo.  请在下方输入版本号并回车以下载开发版本.
-echo.  开发版本的版本号可以在以下链接中获取:
+echo.  Before run this script. Please ensure you have installed java. (17 is the best. 20 is not allowed)
+echo.  Just input the version you want to install below.
+echo.  The development version list of mirai is available here:
 echo.  https://repo.mirai.mamoe.net/snapshots/net/mamoe/mirai-core
 echo.
-set /p version="输入版本号: "
-echo.注: 其中一个库下载耗时较长，请耐心等待
+set /p version="Input version: "
+echo.Tips: Download some libraries may take a long time. Please stand by.
 echo.
-title Mirai一键包打包器 正在下载 %version% - Lapis
+title Mirai OneKey Installer (Downloading %version%) - Lapis
 
 cmd /c gradlew.bat clearLibs pack -Dlapis.version=%version%
 
-title Mirai一键包打包器 - Lapis
+title Mirai OneKey Installer - Lapis
 echo.
-echo.  若提示 BUILD SUCCESSFUL, 一键包已成功打包到 ./build/lapis 文件夹中.
-echo.  若提示 BUILD FAILED, 可能是版本号输入错误或者网络问题.
-echo.  按任意键退出
-pause>nul
+echo.  BUILD SUCCESSFUL means that the pack has put into ./build/lapis folder.
+echo.  FAILED means that you may input a wrong version or your internet connection shutdown.
+pause
